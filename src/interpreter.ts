196c72d232111c7478533d9f01a70c5a3e9ad75f
noukenolife <noukenolife@users.noreply.github.com>
2025-01-02 16:39:40 +0900

    install eslint

--- src/interpreter.ts
+++ src/interpreter.ts
@@ -13,36 +13,41 @@ export class InterpreterImpl implements Interpreter {
     this.client = client;
   }
 
-  private async _interpret<S extends {}, E, A>(state: S, kioa: KIOA<E, A>): Promise<Either<E, [S, A]>> {
+  private async _interpret<S extends object, E, A>(state: S, kioa: KIOA<E, A>): Promise<Either<E, [S, A]>> {
     switch (kioa.kind) {
-      case 'Succeed':
-        const newState = { ...state, [kioa.name]: kioa.value }
-        return Promise.resolve(new Right([ newState, kioa.value ] as [S, A]));
-      case 'Fail': return Promise.resolve(new Left(kioa.error));
-      case 'FlatMap':
+      case 'Succeed': {
+        const newState = {...state, [kioa.name]: kioa.value}
+        return Promise.resolve(new Right([newState, kioa.value] as [S, A]));
+      }
+      case 'Fail': {
+        return Promise.resolve(new Left(kioa.error));
+      }
+      case 'FlatMap': {
         const r1 = await this._interpret(state, kioa.self);
         return (async () => {
           switch (r1.kind) {
-            case 'Left':
-              return r1;
-            case 'Right':
+            case 'Left': return r1;
+            case 'Right': {
               const [s1, a1] = r1.value
               const r2 = await this._interpret(s1, kioa.f(a1, s1));
               return (() => {
                 switch (r2.kind) {
-                  case 'Left':
-                    return r2;
-                  case 'Right':
-                    const [_, a2] = r2.value
+                  case 'Left': return r2;
+                  case 'Right': {
+                    const [, a2] = r2.value
                     const s2 = {...s1, [kioa.name]: a2};
                     return new Right([s2, a2] as [S, A]);
+                  }
                 }
               })();
+            }
           }
         })();
-      case 'GetRecord':
-        const { record } = await this.client.getRecord({ app: kioa.app, id: kioa.id });
-        return new Right([ { ...state, [kioa.name]: record }, record ] as [S, A]);
+      }
+      case 'GetRecord': {
+        const {record} = await this.client.getRecord({app: kioa.app, id: kioa.id});
+        return new Right([{...state, [kioa.name]: record}, record] as [S, A]);
+      }
     }
   }
 
@@ -50,9 +55,10 @@ export class InterpreterImpl implements Interpreter {
     const result = await this._interpret({}, kioa);
     switch (result.kind) {
       case "Left": return result;
-      case "Right":
-        const [_, a] = result.value
+      case "Right": {
+        const [, a] = result.value
         return new Right(a);
+      }
     }
   }
 }
--- src/kio.ts
+++ src/kio.ts
@@ -5,7 +5,7 @@ export type KIOS<T extends string, A> = {
   readonly [K in T]: A
 };
 
-export class KIO<S extends {}, E, A> {
+export class KIO<S extends object, E, A> {
   private kioa: KIOA<E, A>;
 
   private constructor(kioa: KIOA<E, A>) {
@@ -16,11 +16,11 @@ export class KIO<S extends {}, E, A> {
     return (a) => new KIO({ name, kind: 'Succeed', value: a });
   }
 
-  static fail<E>(e: E): KIO<{}, E, unknown> {
+  static fail<E>(e: E): KIO<object, E, unknown> {
     return new KIO({ kind: 'Fail', error: e });
   }
 
-  flatMap<N extends string>(name: N): <S1 extends {}, E1, B>(f: (a: A, s: S) => KIO<S1, E1, B>) => KIO<S & KIOS<N, B>, E1, B> {
+  flatMap<N extends string>(name: N): <S1 extends object, E1, B>(f: (a: A, s: S) => KIO<S1, E1, B>) => KIO<S & KIOS<N, B>, E1, B> {
     return (f) => new KIO({
       kind: 'FlatMap',
       name,
@@ -35,7 +35,7 @@ export class KIO<S extends {}, E, A> {
     });
   }
 
-  getRecord<N extends string>(name: N): <R extends {}>(args: { app: number | string, id: number | string }) => KIO<S & KIOS<N, R>, E, R> {
+  getRecord<N extends string>(name: N): <R extends object>(args: { app: number | string, id: number | string }) => KIO<S & KIOS<N, R>, E, R> {
     return (args) => this.flatMap(name)(() => new KIO({
       kind: 'GetRecord',
       name,
