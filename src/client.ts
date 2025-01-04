60ef93115f1882c6bf41da351f294e2d9eb87bcb
noukenolife <noukenolife@users.noreply.github.com>
2025-01-04 21:26:11 +0900

    implement client getRecords

--- src/client.ts
+++ src/client.ts
@@ -1,10 +1,47 @@
+import { KintoneRestAPIClient } from "@kintone/rest-api-client";
+
+export type RevisionField = { $revision: { value: string | number } };
+export type GetRecordParams = {
+  app: string | number;
+  id: string | number;
+};
+export type GetRecordResponseRecord = { [k: string]: unknown } & RevisionField;
+export type GetRecordResponse = {
+  record: GetRecordResponseRecord;
+};
+export type GetRecordsParams = {
+  app: string | number;
+  fields?: string[];
+  query?: string;
+  totalCount?: boolean;
+};
+export type GetRecordsResponse = ({ [k: string]: unknown } & RevisionField)[];
+
 export interface KintoneClient {
-  getRecord<
-    T extends { [k: string]: unknown } & {
-      $revision: { value: string | number };
-    },
-  >(params: {
-    app: string | number;
-    id: string | number;
-  }): Promise<{ record: T }>;
+  getRecord(params: GetRecordParams): Promise<GetRecordResponse>;
+  getRecords(params: GetRecordsParams): Promise<GetRecordsResponse>;
+}
+
+export class KintoneClientImpl implements KintoneClient {
+  private client: KintoneRestAPIClient;
+
+  constructor(client: KintoneRestAPIClient) {
+    this.client = client;
+  }
+
+  async getRecord<T extends GetRecordResponseRecord>(
+    params: GetRecordParams,
+  ): Promise<GetRecordResponse> {
+    const result = await this.client.record.getRecord(params);
+    return result as unknown as { record: T };
+  }
+
+  async getRecords(params: GetRecordsParams): Promise<GetRecordsResponse> {
+    const _params: GetRecordsParams = {
+      ...params,
+      fields: params.fields ? [...params.fields, "$revision"] : undefined,
+    };
+    const { records } = await this.client.record.getRecords(_params);
+    return records as unknown as GetRecordsResponse;
+  }
 }
