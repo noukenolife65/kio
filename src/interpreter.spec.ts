ff4937da33f0c2d0ee84952e20dd641c63bda2ad
noukenolife <noukenolife@users.noreply.github.com>
2025-01-05 11:34:35 +0900

    refactor interpreter test

--- src/interpreter.spec.ts
+++ src/interpreter.spec.ts
@@ -32,66 +32,70 @@ describe("InterpreterImpl", () => {
 
   const interpreter = new InterpreterImpl(client);
   describe("interpret", () => {
-    it("Succeed", async () => {
-      const result = await KIO.succeed("succeed")(1)
-        .map("map")((a, s) => {
-          expect(s).toStrictEqual({ succeed: new KValue(1) });
-          return a;
-        })
-        .commit(interpreter);
-      expect(result).toStrictEqual(new Right(1));
-    });
-    it("Fail", async () => {
-      const result = await KIO.fail("error")
-        .map("map")(() => {
-          expect.fail();
-        })
-        .commit(interpreter);
-      expect(result).toStrictEqual(new Left("error"));
-    });
-    it("FlatMap", async () => {
-      const success = await KIO.succeed("succeed1")(1)
-        .flatMap("flatMap")((a, s) => {
-          expect(s).toStrictEqual({ succeed1: new KValue(1) });
-          return KIO.succeed("succeed2")(a.value + 1);
-        })
-        .commit(interpreter);
-      expect(success).toStrictEqual(new Right(2));
+    describe("Basic Operations", () => {
+      it("Succeed", async () => {
+        const result = await KIO.succeed("succeed")(1)
+          .map("map")((a, s) => {
+            expect(s).toStrictEqual({ succeed: new KValue(1) });
+            return a;
+          })
+          .commit(interpreter);
+        expect(result).toStrictEqual(new Right(1));
+      });
+      it("Fail", async () => {
+        const result = await KIO.fail("error")
+          .map("map")(() => {
+            expect.fail();
+          })
+          .commit(interpreter);
+        expect(result).toStrictEqual(new Left("error"));
+      });
+      it("FlatMap", async () => {
+        const success = await KIO.succeed("succeed1")(1)
+          .flatMap("flatMap")((a, s) => {
+            expect(s).toStrictEqual({ succeed1: new KValue(1) });
+            return KIO.succeed("succeed2")(a.value + 1);
+          })
+          .commit(interpreter);
+        expect(success).toStrictEqual(new Right(2));
 
-      const failure = await KIO.succeed("succeed1")(1)
-        .flatMap("flatMap1")(() => KIO.fail("error"))
-        .flatMap("flatMap2")(() => {
-          return KIO.succeed("succeed2")(1);
-        })
-        .commit(interpreter);
-      expect(failure).toStrictEqual(new Left("error"));
+        const failure = await KIO.succeed("succeed1")(1)
+          .flatMap("flatMap1")(() => KIO.fail("error"))
+          .flatMap("flatMap2")(() => {
+            return KIO.succeed("succeed2")(1);
+          })
+          .commit(interpreter);
+        expect(failure).toStrictEqual(new Left("error"));
+      });
     });
-    it("GetRecord", async () => {
-      const expectedRecord = { test: { value: 1 }, $revision: { value: 2 } };
-      const result = await KIO.succeed("succeed")(1)
-        .flatMap("record")(() =>
-          KIO.getRecord("record")<typeof expectedRecord>({ app: 1, id: 1 }),
-        )
-        .map("map")((a, s) => {
-          expect(s).toStrictEqual({
-            succeed: new KValue(1),
-            record: new KRecord(expectedRecord, 1, 2),
-          });
-          return a;
-        })
-        .commit(interpreter);
-      expect(result).toStrictEqual(new Right(expectedRecord));
-    });
-    it("GetRecords", async () => {
-      const expectedRecord = { test: { value: 1 }, $revision: { value: 2 } };
-      const result = await KIO.succeed("succeed")(1)
-        .flatMap("records")(() =>
-          KIO.getRecords("records")<typeof expectedRecord>({
-            app: 1,
-          }),
-        )
-        .commit(interpreter);
-      expect(result).toStrictEqual(new Right([expectedRecord]));
+    describe("Kintone Operations", () => {
+      it("GetRecord", async () => {
+        const expectedRecord = { test: { value: 1 }, $revision: { value: 2 } };
+        const result = await KIO.succeed("succeed")(1)
+          .flatMap("record")(() =>
+            KIO.getRecord("record")<typeof expectedRecord>({ app: 1, id: 1 }),
+          )
+          .map("map")((a, s) => {
+            expect(s).toStrictEqual({
+              succeed: new KValue(1),
+              record: new KRecord(expectedRecord, 1, 2),
+            });
+            return a;
+          })
+          .commit(interpreter);
+        expect(result).toStrictEqual(new Right(expectedRecord));
+      });
+      it("GetRecords", async () => {
+        const expectedRecord = { test: { value: 1 }, $revision: { value: 2 } };
+        const result = await KIO.succeed("succeed")(1)
+          .flatMap("records")(() =>
+            KIO.getRecords("records")<typeof expectedRecord>({
+              app: 1,
+            }),
+          )
+          .commit(interpreter);
+        expect(result).toStrictEqual(new Right([expectedRecord]));
+      });
     });
   });
 });
