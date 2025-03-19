a5d75de964e7d491a639cdbaa77ad839ee7fc014
noukenolife <noukenolife@users.noreply.github.com>
2025-03-19 21:15:20 +0900

    update package.json

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,6 @@ export default defineConfig({
   build: {
     lib: {
       entry: "src/index.ts",
-      name: "kio",
-      fileName: "kio",
       formats: ["es", "cjs"],
     },
     rollupOptions: {
