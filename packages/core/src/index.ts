ea033a1f9dda0e79b47f25d81b6ce3b959b75448
noukenolife <182641368+noukenolife65@users.noreply.github.com>
2025-07-21 10:56:45 +0000

    implement pipe

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,4 +1,5 @@
 import { KIO } from "./kio.ts";
 import { createRunner } from "./runner/promise/runner.ts";
+import { pipe } from "./pipe.ts";
 
-export { KIO, createRunner };
+export { KIO, createRunner, pipe };
